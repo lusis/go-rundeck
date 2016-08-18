093fd6d17371af8b48de1ed3c500489cdd093af9
John E Vincent <lusis.org+github.com@gmail.com>
2016-08-18 09:43:17 -0400

    more work on unifying cli options

--- src/rundeck-list-running-executions/main.go
+++ src/rundeck-list-running-executions/main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/olekukonko/tablewriter"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 	rundeck "rundeck.v17"
 )
 
 var (
-	project = kingpin.Arg("project", "").Required().String()
+	project     = kingpin.Arg("project", "").Required().String()
+	formatUsage = fmt.Sprintf("Format to show results [table, csv, list (ids only - useful for piping)]")
+	format      = kingpin.Flag("format", formatUsage).Short('F').Default("table").Enum("table", "list", "csv")
+	sep         = kingpin.Flag("separator", "separator for csv output").Default(",").String()
+	header      = kingpin.Flag("headers", "add headers for csv output").Default("false").Bool()
 )
 
 func main() {
@@ -20,8 +25,36 @@ func main() {
 		fmt.Printf("%s\n", err)
 		os.Exit(1)
 	} else {
-		fmt.Printf("%+v\n", res)
-		//fmt.Printf("Job %s is %s", res.Executions[0].ID, res.Executions[0].Status)
+		if *format == "table" {
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{
+				"ID",
+				"Status",
+				"User",
+			})
+			for _, d := range res.Executions {
+				table.Append([]string{
+					d.ID,
+					d.Status,
+					d.User,
+				})
+			}
+			table.Render()
+		} else if *format == "csv" {
+			if *header == true {
+				fmt.Printf("ID%s%Status%sUser", *sep)
+			}
+			for _, d := range res.Executions {
+				fmt.Printf("%s%s%s%s%s\n", d.ID, *sep, d.Status, *sep, d.User)
+			}
+		} else if *format == "list" {
+			for _, d := range res.Executions {
+				fmt.Printf("%s\n", d.ID)
+			}
+		} else {
+			fmt.Printf("Unknown output format: %s\n", *format)
+			os.Exit(1)
+		}
 		os.Exit(0)
 	}
 }
