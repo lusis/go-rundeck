7f0103bc69855b6c5b4f4e018f95bcea41f7ad48
John E Vincent <lusis.org+github.com@gmail.com>
2018-01-07 14:46:40 -0500

    export project archive

--- cmd/rundeck/cmds/export_project.go
+++ cmd/rundeck/cmds/export_project.go
@@ -10,7 +10,14 @@ import (
 )
 
 var (
-	projectExportFile string
+	projectExportAll          bool
+	projectExportExecutionIDs []string
+	projectExportJobs         bool
+	projectExportExecutions   bool
+	projectExportConfigs      bool
+	projectExportAcls         bool
+	projectExportReadmes      bool
+	projectExportFile         string
 )
 
 func exportProjectFunc(cmd *cobra.Command, args []string) error {
@@ -20,18 +27,37 @@ func exportProjectFunc(cmd *cobra.Command, args []string) error {
 	if fErr != nil {
 		return fErr
 	}
-	return cli.Client.GetProjectArchiveExport(projectName, f, rundeck.ProjectExportAll(true))
+	opts := []rundeck.ProjectExportOption{
+		rundeck.ProjectExportAll(projectExportAll),
+		rundeck.ProjectExportConfigs(projectExportConfigs),
+		rundeck.ProjectExportAcls(projectExportAcls),
+		rundeck.ProjectExportJobs(projectExportJobs),
+		rundeck.ProjectExportReadmes(projectExportReadmes),
+	}
+	if len(projectExportExecutionIDs) > 0 {
+		opts = append(opts, rundeck.ProjectExportExecutionIDs(projectExportExecutionIDs...)) // nolint: ineffassign
+	}
+
+	return cli.Client.GetProjectArchiveExport(projectName, f, opts...)
 }
 
 func exportProjectCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "export project-name [-f destination-file]",
+		Use:   "export project-name [-o destination-file]",
 		Short: "exports a project from a rundeck server",
 		Args:  cobra.MinimumNArgs(1),
 		RunE:  exportProjectFunc,
 	}
 	rootCmd := cli.New(cmd)
 	rootCmd.ResetFlags()
-	rootCmd.Flags().StringVarP(&projectExportFile, "file", "f", "project-export.zip", "destination filename for the export")
+
+	rootCmd.Flags().StringVarP(&projectExportFile, "output-file", "o", "project-export.zip", "destination filename for the export")
+	rootCmd.Flags().BoolVar(&projectExportAll, "all", true, "export everything")
+	rootCmd.Flags().StringSliceVarP(&projectExportExecutionIDs, "ids", "i", nil, "specify specific execution ids to export")
+	rootCmd.Flags().BoolVar(&projectExportAcls, "acls", true, "export acls")
+	rootCmd.Flags().BoolVar(&projectExportConfigs, "configs", true, "export configs")
+	rootCmd.Flags().BoolVar(&projectExportExecutions, "executions", true, "export executions")
+	rootCmd.Flags().BoolVar(&projectExportJobs, "jobs", true, "export jobs")
+	rootCmd.Flags().BoolVar(&projectExportReadmes, "readmes", true, "export readmes")
 	return rootCmd
 }
