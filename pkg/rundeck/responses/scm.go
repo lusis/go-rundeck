a7177676a713aedf630e5796fe7c9eb684a47367
John E Vincent <lusis.org+github.com@gmail.com>
2018-01-08 17:21:39 -0500

    more scm work

--- pkg/rundeck/responses/scm.go
+++ pkg/rundeck/responses/scm.go
@@ -1,5 +1,12 @@
 package responses
 
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+)
+
 // SCMResponse is current a placeholder response for SCM
 // http://rundeck.org/docs/api/index.html#scm
 type SCMResponse struct{}
@@ -14,6 +21,27 @@ type ListSCMPluginsResponse struct {
 	SCMResponse
 	Integration string               `json:"integration"`
 	Plugins     []*SCMPluginResponse `json:"plugins"`
+}
+
+// ListSCMPluginsResponseImportTestFile is the test data for list scm plugins response for import scm
+const ListSCMPluginsResponseImportTestFile = "list_scm_plugins_import.json"
+
+// ListSCMPluginsResponseExportTestFile is the test data for list scm plugins response for export scm
+const ListSCMPluginsResponseExportTestFile = "list_scm_plugins_export.json"
+
+// FromReader returns a ListSCMPluginsResponse from an io.Reader
+func (a *ListSCMPluginsResponse) FromReader(i io.Reader) error {
+	b, err := ioutil.ReadAll(i)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, a)
+}
+
+// FromBytes returns a ListSCMPluginsResponse from a byte slice
+func (a *ListSCMPluginsResponse) FromBytes(f []byte) error {
+	file := bytes.NewReader(f)
+	return a.FromReader(file)
 }
 
 // SCMPluginResponse is an individual Plugin entry in ListSCMPluginsResponse
@@ -45,29 +73,31 @@ type GetSCMPluginInputFieldsResponse struct {
 	Type        string `json:"type"`
 }
 
+// GetSCMPluginInputFieldsResponseImportTestData is test data for GetSCMPluginInputFieldsResponse import plugins
+const GetSCMPluginInputFieldsResponseImportTestData = "get_scm_input_plugin_fields_import.json"
+
+// GetSCMPluginInputFieldsResponseExportTestData is test data for GetSCMPluginInputFieldsResponse export plugins
+const GetSCMPluginInputFieldsResponseExportTestData = "get_scm_input_plugin_fields_export.json"
+
+// FromReader returns a GetSCMPluginInputFieldsResponse from an io.Reader
+func (a *GetSCMPluginInputFieldsResponse) FromReader(i io.Reader) error {
+	b, err := ioutil.ReadAll(i)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, a)
+}
+
+// FromBytes returns a GetSCMPluginInputFieldsResponse from a byte slice
+func (a *GetSCMPluginInputFieldsResponse) FromBytes(f []byte) error {
+	file := bytes.NewReader(f)
+	return a.FromReader(file)
+}
+
 // SCMPluginForProjectResponse is the response for setting up, enabling or disabling an scm plugin for a project
 // http://rundeck.org/docs/api/index.html#setup-scm-plugin-for-a-project
 // http://rundeck.org/docs/api/index.html#enable-scm-plugin-for-a-project
 // http://rundeck.org/docs/api/index.html#disable-scm-plugin-for-a-project
-/*
-failed
-{
-  "message": "Some input values were not valid.",
-  "nextAction": null,
-  "success": false,
-  "validationErrors": {
-    "dir": "required",
-    "url": "required"
-  }
-}
-succeeded
-{
-  "message": "$string",
-  "nextAction": null,
-  "success": true,
-  "validationErrors": null
-}
-*/
 type SCMPluginForProjectResponse struct {
 	SCMResponse
 	Message          string            `json:"message"`
@@ -76,17 +106,35 @@ type SCMPluginForProjectResponse struct {
 	ValidationErrors map[string]string `json:"validationErrors,omitempty"`
 }
 
+// SCMPluginForProjectResponseEnableImportTestFile is test data for enabling an scm import plugin
+const SCMPluginForProjectResponseEnableImportTestFile = "enable_scm_plugin_import.json"
+
+// SCMPluginForProjectResponseDisableImportTestFile is test data for disabling an scm import plugin
+const SCMPluginForProjectResponseDisableImportTestFile = "disable_scm_plugin_import.json"
+
+// SCMPluginForProjectResponseEnableExportTestFile is test data for enabling an scm export plugin
+const SCMPluginForProjectResponseEnableExportTestFile = "enable_scm_plugin_export.json"
+
+// SCMPluginForProjectResponseDisableExportTestFile is test data for disabling an scm export plugin
+const SCMPluginForProjectResponseDisableExportTestFile = "disable_scm_plugin_export.json"
+
+// FromReader returns a SCMPluginForProjectResponse from an io.Reader
+func (a *SCMPluginForProjectResponse) FromReader(i io.Reader) error {
+	b, err := ioutil.ReadAll(i)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, a)
+}
+
+// FromBytes returns a SCMPluginForProjectResponse from a byte slice
+func (a *SCMPluginForProjectResponse) FromBytes(f []byte) error {
+	file := bytes.NewReader(f)
+	return a.FromReader(file)
+}
+
 // GetProjectSCMStatusResponse is the response for getting a project's scm status
 // http://rundeck.org/docs/api/index.html#get-project-scm-status
-/*
-{
-  "actions": ['action1','action2',..],
-  "integration": "$integration",
-  "message": null,
-  "project": "$project",
-  "synchState": "$state"
-}
-*/
 type GetProjectSCMStatusResponse struct {
 	SCMResponse
 	Actions     []string `json:"actions"`
@@ -94,6 +142,27 @@ type GetProjectSCMStatusResponse struct {
 	Message     string   `json:"message,omitempty"`
 	Project     string   `json:"project"`
 	SynchState  string   `json:"synchState"`
+}
+
+// GetProjectSCMStatusResponseImportTestFile is test data for a GetProjectSCMStatusResponse import plugin
+const GetProjectSCMStatusResponseImportTestFile = "get_project_scm_status_import.json"
+
+// GetProjectSCMStatusResponseExportTestFile is test data for a GetProjectSCMStatusResponse export plugin
+const GetProjectSCMStatusResponseExportTestFile = "get_project_scm_status_export.json"
+
+// FromReader returns a GetProjectSCMStatusResponse from an io.Reader
+func (a *GetProjectSCMStatusResponse) FromReader(i io.Reader) error {
+	b, err := ioutil.ReadAll(i)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, a)
+}
+
+// FromBytes returns a GetProjectSCMStatusResponse from a byte slice
+func (a *GetProjectSCMStatusResponse) FromBytes(f []byte) error {
+	file := bytes.NewReader(f)
+	return a.FromReader(file)
 }
 
 // GetProjectSCMConfigResponse is the response for getting a project's scm config
@@ -117,6 +186,27 @@ type GetProjectSCMConfigResponse struct {
 	Integration string            `json:"integration"`
 	Project     string            `json:"project"`
 	Type        string            `json:"type"`
+}
+
+// GetProjectSCMConfigResponseImportTestFile is testdata for GetProjectSCMConfigResponse for import plugins
+const GetProjectSCMConfigResponseImportTestFile = "get_project_scm_config_import.json"
+
+// GetProjectSCMConfigResponseExportTestFile is testdata for GetProjectSCMConfigResponse for export plugins
+const GetProjectSCMConfigResponseExportTestFile = "get_project_scm_config_export.json"
+
+// FromReader returns a GetProjectSCMConfigResponse from an io.Reader
+func (a *GetProjectSCMConfigResponse) FromReader(i io.Reader) error {
+	b, err := ioutil.ReadAll(i)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, a)
+}
+
+// FromBytes returns a GetProjectSCMConfigResponse from a byte slice
+func (a *GetProjectSCMConfigResponse) FromBytes(f []byte) error {
+	file := bytes.NewReader(f)
+	return a.FromReader(file)
 }
 
 // GetSCMActionInputFieldsResponse is the response for getting a project's scm action input fields
@@ -151,6 +241,21 @@ type GetSCMActionInputFieldsResponse struct {
 	} `json:"exportItems,omitempty"`
 }
 
+// FromReader returns a GetSCMActionInputFieldsResponse from an io.Reader
+func (a *GetSCMActionInputFieldsResponse) FromReader(i io.Reader) error {
+	b, err := ioutil.ReadAll(i)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, a)
+}
+
+// FromBytes returns a GetSCMActionInputFieldsResponse from a byte slice
+func (a *GetSCMActionInputFieldsResponse) FromBytes(f []byte) error {
+	file := bytes.NewReader(f)
+	return a.FromReader(file)
+}
+
 // GetJobSCMStatusResponse is the response for getting a job's scm status
 // http://rundeck.org/docs/api/index.html#get-job-scm-status
 // Note: import status will not include any actions for the job, refer to the Project status to list import actions.
@@ -169,6 +274,21 @@ type GetJobSCMStatusResponse struct {
 	Message     string `json:"message"`
 	Project     string `json:"project"`
 	SynchState  string `json:"synchState"`
+}
+
+// FromReader returns a GetJobSCMStatusResponse from an io.Reader
+func (a *GetJobSCMStatusResponse) FromReader(i io.Reader) error {
+	b, err := ioutil.ReadAll(i)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, a)
+}
+
+// FromBytes returns a GetJobSCMStatusResponse from a byte slice
+func (a *GetJobSCMStatusResponse) FromBytes(f []byte) error {
+	file := bytes.NewReader(f)
+	return a.FromReader(file)
 }
 
 // GetJobSCMDiffResponse is the response for a job scm diff
@@ -195,6 +315,21 @@ type GetJobSCMDiffResponse struct {
 	Project     string `json:"project"`
 }
 
+// FromReader returns a GetJobSCMDiffResponse from an io.Reader
+func (a *GetJobSCMDiffResponse) FromReader(i io.Reader) error {
+	b, err := ioutil.ReadAll(i)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, a)
+}
+
+// FromBytes returns a GetJobSCMDiffResponse from a byte slice
+func (a *GetJobSCMDiffResponse) FromBytes(f []byte) error {
+	file := bytes.NewReader(f)
+	return a.FromReader(file)
+}
+
 // PerformJobSCMActionResponse is the response for performing a job scm action
 // http://rundeck.org/docs/api/index.html#perform-job-scm-action
 type PerformJobSCMActionResponse struct {
@@ -202,4 +337,19 @@ type PerformJobSCMActionResponse struct {
 	Input struct {
 		Message string `json:"message"`
 	} `json:"input"`
+}
+
+// FromReader returns a PerformJobSCMActionResponse from an io.Reader
+func (a *PerformJobSCMActionResponse) FromReader(i io.Reader) error {
+	b, err := ioutil.ReadAll(i)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, a)
+}
+
+// FromBytes returns a PerformJobSCMActionResponse from a byte slice
+func (a *PerformJobSCMActionResponse) FromBytes(f []byte) error {
+	file := bytes.NewReader(f)
+	return a.FromReader(file)
 }
--- pkg/rundeck/responses/scm_test.go
+++ pkg/rundeck/responses/scm_test.go
@@ -3,12 +3,38 @@ package responses
 import (
 	"testing"
 
+	"github.com/lusis/go-rundeck/pkg/rundeck/responses/testdata"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestListSCMPluginsResponse(t *testing.T) {
-	obj := ListSCMPluginsResponse{}
+func TestListSCMPluginsResponseImport(t *testing.T) {
+	obj := &ListSCMPluginsResponse{}
+	data, dataErr := testdata.GetBytes(ListSCMPluginsResponseImportTestFile)
+	if dataErr != nil {
+		t.Fatalf(dataErr.Error())
+	}
+
+	err := obj.FromBytes(data)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
 	assert.Implements(t, (*VersionedResponse)(nil), obj)
+	assert.NotNil(t, obj)
+}
+
+func TestListSCMPluginsResponseExport(t *testing.T) {
+	obj := &ListSCMPluginsResponse{}
+	data, dataErr := testdata.GetBytes(ListSCMPluginsResponseExportTestFile)
+	if dataErr != nil {
+		t.Fatalf(dataErr.Error())
+	}
+
+	err := obj.FromBytes(data)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	assert.Implements(t, (*VersionedResponse)(nil), obj)
+	assert.NotNil(t, obj)
 }
 
 func TestSCMPluginResponse(t *testing.T) {
@@ -16,7 +42,122 @@ func TestSCMPluginResponse(t *testing.T) {
 	assert.Implements(t, (*VersionedResponse)(nil), obj)
 }
 
-func TestGetSCMPluginInputFieldsResponse(t *testing.T) {
-	obj := GetSCMPluginInputFieldsResponse{}
+func TestGetSCMPluginInputFieldsResponseImport(t *testing.T) {
+	obj := &GetSCMPluginInputFieldsResponse{}
+	data, dataErr := testdata.GetBytes(GetSCMPluginInputFieldsResponseImportTestData)
+	if dataErr != nil {
+		t.Fatalf(dataErr.Error())
+	}
+
+	err := obj.FromBytes(data)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
 	assert.Implements(t, (*VersionedResponse)(nil), obj)
+	assert.NotNil(t, obj)
+}
+
+func TestGetSCMPluginInputFieldsResponseExport(t *testing.T) {
+	obj := &GetSCMPluginInputFieldsResponse{}
+	data, dataErr := testdata.GetBytes(GetSCMPluginInputFieldsResponseExportTestData)
+	if dataErr != nil {
+		t.Fatalf(dataErr.Error())
+	}
+
+	err := obj.FromBytes(data)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	assert.Implements(t, (*VersionedResponse)(nil), obj)
+	assert.NotNil(t, obj)
+}
+
+func TestSCMPluginForProjectResponseEnableImport(t *testing.T) {
+	obj := &SCMPluginForProjectResponse{}
+	data, dataErr := testdata.GetBytes(SCMPluginForProjectResponseEnableImportTestFile)
+	if dataErr != nil {
+		t.Fatalf(dataErr.Error())
+	}
+
+	err := obj.FromBytes(data)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	assert.Implements(t, (*VersionedResponse)(nil), obj)
+	assert.NotNil(t, obj)
+}
+
+func TestSCMPluginForProjectResponseDisableImport(t *testing.T) {
+	obj := &SCMPluginForProjectResponse{}
+	data, dataErr := testdata.GetBytes(SCMPluginForProjectResponseDisableImportTestFile)
+	if dataErr != nil {
+		t.Fatalf(dataErr.Error())
+	}
+
+	err := obj.FromBytes(data)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	assert.Implements(t, (*VersionedResponse)(nil), obj)
+	assert.NotNil(t, obj)
+}
+
+func TestSCMPluginForProjectResponseEnableExport(t *testing.T) {
+	obj := &SCMPluginForProjectResponse{}
+	data, dataErr := testdata.GetBytes(SCMPluginForProjectResponseEnableExportTestFile)
+	if dataErr != nil {
+		t.Fatalf(dataErr.Error())
+	}
+
+	err := obj.FromBytes(data)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	assert.Implements(t, (*VersionedResponse)(nil), obj)
+	assert.NotNil(t, obj)
+}
+
+func TestSCMPluginForProjectResponseDisableExport(t *testing.T) {
+	obj := &SCMPluginForProjectResponse{}
+	data, dataErr := testdata.GetBytes(SCMPluginForProjectResponseDisableExportTestFile)
+	if dataErr != nil {
+		t.Fatalf(dataErr.Error())
+	}
+
+	err := obj.FromBytes(data)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	assert.Implements(t, (*VersionedResponse)(nil), obj)
+	assert.NotNil(t, obj)
+}
+
+func TestGetProjectSCMStatusResponseImport(t *testing.T) {
+	obj := &GetProjectSCMStatusResponse{}
+	data, dataErr := testdata.GetBytes(GetProjectSCMStatusResponseImportTestFile)
+	if dataErr != nil {
+		t.Fatalf(dataErr.Error())
+	}
+
+	err := obj.FromBytes(data)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	assert.Implements(t, (*VersionedResponse)(nil), obj)
+	assert.NotNil(t, obj)
+}
+
+func TestGetProjectSCMStatusResponseExport(t *testing.T) {
+	obj := &GetProjectSCMStatusResponse{}
+	data, dataErr := testdata.GetBytes(GetProjectSCMStatusResponseExportTestFile)
+	if dataErr != nil {
+		t.Fatalf(dataErr.Error())
+	}
+
+	err := obj.FromBytes(data)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	assert.Implements(t, (*VersionedResponse)(nil), obj)
+	assert.NotNil(t, obj)
 }
