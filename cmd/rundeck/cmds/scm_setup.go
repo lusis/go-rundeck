c837908ea6d95a92de6a0ebd40b50991f6e9e78c
John E Vincent <lusis.org+github.com@gmail.com>
2018-02-12 17:05:40 -0500

    quick example

--- cmd/rundeck/cmds/scm_setup.go
+++ cmd/rundeck/cmds/scm_setup.go
@@ -19,10 +19,11 @@ func setupProjectSCMFunc(cmd *cobra.Command, args []string) error {
 }
 func setupProjectSCMCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "setup project-name integration plugin-type -o setting=value [-o setting=value]",
-		Short: "sets up scm integration for a rundeck project",
-		Args:  cobra.MinimumNArgs(3),
-		RunE:  setupProjectSCMFunc,
+		Use:     "setup project-name integration plugin-type -o setting=value [-o setting=value]",
+		Short:   "sets up scm integration for a rundeck project",
+		Example: "setup fooprj import git-import -o dir=/var/tmp/fooprj -o url=/home/rundeck-import.git/ -o pathTemplate=\\${job.group}\\${job.name}-\\${job.id}.\\${config.format} -o branch=master -o format=yaml -o strictHostKeyChecking=no",
+		Args:    cobra.MinimumNArgs(3),
+		RunE:    setupProjectSCMFunc,
 	}
 
 	rootCmd := cli.New(cmd)
